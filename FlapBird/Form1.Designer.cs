14e553cd3d46790bd22a0b971d2b0fb55ff85ec9
Carlos Eduardo <carlos_duhduh5569@hotmail.com>
2022-06-06 10:49:00 -0300

    commit inicial

---
+++ FlapBird/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace FlapBird
+{
+    partial class flapbird
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lb_score = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.bird = new System.Windows.Forms.PictureBox();
+            this.pipetop = new System.Windows.Forms.PictureBox();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.pipebuttom = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipetop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipebuttom)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lb_score
+            // 
+            this.lb_score.AutoSize = true;
+            this.lb_score.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(216)))), ((int)(((byte)(149)))));
+            this.lb_score.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_score.Location = new System.Drawing.Point(6, 428);
+            this.lb_score.Name = "lb_score";
+            this.lb_score.Size = new System.Drawing.Size(118, 25);
+            this.lb_score.TabIndex = 4;
+            this.lb_score.Text = "SCORE: 0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimerEvents);
+            // 
+            // bird
+            // 
+            this.bird.BackColor = System.Drawing.Color.Transparent;
+            this.bird.Image = global::FlapBird.Properties.Resources.stop;
+            this.bird.Location = new System.Drawing.Point(10, 100);
+            this.bird.Name = "bird";
+            this.bird.Size = new System.Drawing.Size(50, 50);
+            this.bird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bird.TabIndex = 0;
+            this.bird.TabStop = false;
+            // 
+            // pipetop
+            // 
+            this.pipetop.BackColor = System.Drawing.Color.Transparent;
+            this.pipetop.Image = global::FlapBird.Properties.Resources.pipedown;
+            this.pipetop.Location = new System.Drawing.Point(311, 0);
+            this.pipetop.Name = "pipetop";
+            this.pipetop.Size = new System.Drawing.Size(50, 250);
+            this.pipetop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipetop.TabIndex = 3;
+            this.pipetop.TabStop = false;
+            // 
+            // ground
+            // 
+            this.ground.BackColor = System.Drawing.Color.Transparent;
+            this.ground.Image = global::FlapBird.Properties.Resources.ground;
+            this.ground.Location = new System.Drawing.Point(-1, 416);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(387, 50);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ground.TabIndex = 1;
+            this.ground.TabStop = false;
+            // 
+            // pipebuttom
+            // 
+            this.pipebuttom.BackColor = System.Drawing.Color.Transparent;
+            this.pipebuttom.Image = global::FlapBird.Properties.Resources.pipe;
+            this.pipebuttom.Location = new System.Drawing.Point(311, 350);
+            this.pipebuttom.Name = "pipebuttom";
+            this.pipebuttom.Size = new System.Drawing.Size(50, 250);
+            this.pipebuttom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipebuttom.TabIndex = 2;
+            this.pipebuttom.TabStop = false;
+            // 
+            // flapbird
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(384, 461);
+            this.Controls.Add(this.bird);
+            this.Controls.Add(this.lb_score);
+            this.Controls.Add(this.pipetop);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.pipebuttom);
+            this.MaximumSize = new System.Drawing.Size(400, 500);
+            this.MinimumSize = new System.Drawing.Size(400, 500);
+            this.Name = "flapbird";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FlapBird";
+            this.Load += new System.EventHandler(this.flapbird_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gamekeyisDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gamekeyisUp);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.previerKeyDown_func);
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipetop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipebuttom)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox bird;
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.PictureBox pipebuttom;
+        private System.Windows.Forms.Label lb_score;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.PictureBox pipetop;
+    }
+}
+
---
+++ FlapBird/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlapBird
+{
+    public partial class flapbird : Form
+    {
+        int pipeSpeed = 5;
+        int gravity = 5;
+        int score = 0;
+        Random random = new Random();
+        int auxrnd;
+        public flapbird()
+        {
+            InitializeComponent();
+            auxrnd = random.Next(1, 180);
+        }
+
+
+        private void gameTimerEvents(object sender, EventArgs e)
+        {
+            bird.Top += gravity;
+            pipetop.Left -= pipeSpeed;
+            pipebuttom.Left -= pipeSpeed;
+            lb_score.Text = "SCORE: " + score + "rnd: " + auxrnd;
+
+            if (pipebuttom.Left < -50)
+            {
+                pipebuttom.Left = 400;
+                pipetop.Left = 400;
+                score++;
+
+                auxrnd = random.Next(150);
+
+
+                pipetop.Size = new Size(pipetop.Size.Width, 70+auxrnd);
+                
+                pipebuttom.Location = new Point(pipebuttom.Location.X, 170+auxrnd);
+                //Height = auxrnd;
+                
+            }
+            if (bird.Bounds.IntersectsWith(pipebuttom.Bounds) ||
+                bird.Bounds.IntersectsWith(pipetop.Bounds) ||
+                bird.Bounds.IntersectsWith(ground.Bounds) ||
+                bird.Top < -25
+                )
+            {
+                endGame();
+            }
+        }
+
+        private void gamekeyisDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = -5;
+                bird.Image = FlapBird.Properties.Resources.up;
+            }
+        }
+
+        private void gamekeyisUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space) gravity = 5;
+            bird.Image = FlapBird.Properties.Resources.down;
+        }
+        private void flapbird_Load(object sender, EventArgs e)
+        {
+            bird.Image = FlapBird.Properties.Resources.stop;
+        }
+        private void endGame()
+        {
+            gameTimer.Stop();
+            lb_score.Text += "    GAME OVER";
+
+            if(MessageBox.Show("Jogar Novamente?", "GAME OVER", MessageBoxButtons.YesNo, MessageBoxIcon.None) == DialogResult.Yes)
+            {
+                gameTimer.Start();
+                pipetop.Left = 400;
+                pipebuttom.Left = 400;
+                score = 0;
+                
+                bird.Location = new Point(10,100);
+            }
+            else 
+            {
+                Application.Exit();
+            }
+        }
+
+
+        private void previerKeyDown_func(object sender, PreviewKeyDownEventArgs e)
+        {
+
+        }
+    }
+}
